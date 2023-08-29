46fa4c79428f0d03ad4960f69cba4d4b8dfadb46
Larissa Vieira <57922761+nlarissa36@users.noreply.github.com>
2023-08-29 16:55:36 -0300

    Data 29.08.2023
    Data 29.08.2023

---
+++ Slide_01/slide29.py
@@ -0,0 +1,8 @@
+
+n=15
+#execute n times
+for i in range(0,n):
+    print("Hello %d"%i)
+
+
+
---
+++ Slide_01/slide30.py
@@ -0,0 +1,5 @@
+n=3
+#laço externo executa n vezes
+for i in range(0,n):
+    for j in range(0,n):
+        print("Valor de i:%d, valor de j: %d"%(i,j))
